7e2d97491b9909ecc99786d86b568730e2ff8539
ArthurvanStrien <arthurvanstrien@gmail.com>
2017-02-21 22:21:13 +0100

    finished exercise 3.1 colorRectangles.

--- 3.1_ColorRectangles/src/Rectangle.java
+++ 3.1_ColorRectangles/src/Rectangle.java
@@ -16,20 +16,42 @@ public class Rectangle extends JPanel {
 
     public void paintComponent(Graphics g)
     {
-        Color red = Color.RED;
-        Color green = Color.GREEN;
-        Color blue = Color.BLUE;
-        Color yellow = Color.YELLOW;
-        Color cyan = Color.CYAN;
-        Color magenta = Color.MAGENTA;
-        Color white = Color.WHITE;
-        Color black = Color.BLACK;
-        Color gray = Color.GRAY;
-        Color lightGray = Color.LIGHT_GRAY;
-        Color darkGray = Color.DARK_GRAY;
-        Color orange = Color.ORANGE;
-        Color pink = Color.PINK;
+        super.paintComponent(g);
+        Graphics2D graphics2D = (Graphics2D) g;
+        int xPosition = 0;
+        int yPosition = 0;
 
+        int size = 500;
+        Color colors[] = new Color[13];
+        colors[0] = Color.RED;
+        colors[1] = Color.GREEN;
+        colors[2] = Color.BLUE;
+        colors[3] = Color.YELLOW;
+        colors[4] = Color.CYAN;
+        colors[5] = Color.MAGENTA;
+        colors[6] = Color.WHITE;
+        colors[7] = Color.BLACK;
+        colors[8] = Color.GRAY;
+        colors[9] = Color.LIGHT_GRAY;
+        colors[10] = Color.DARK_GRAY;
+        colors[11] = Color.ORANGE;
+        colors[12] = Color.PINK;
+
+        for(int i = 0; i < 13; i++)
+        {
+            graphics2D.setColor(colors[i]);
+            graphics2D.fillRect(xPosition, yPosition, size, size);
+
+            if((i + 1) % 5 == 0)
+            {
+                xPosition = 0;
+                yPosition = yPosition + size;
+            }
+            else
+            {
+                xPosition = xPosition + size;
+            }
+        }
 
     }
 }
